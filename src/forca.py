bc3502f0fbaca62326b22d62d27bec1e56fed5cb
Luis Eduardo <luiseduardolemos282@gmail.com>
2021-08-08 13:57:18 -0300

    adicionando metodos

--- src/forca.py
+++ src/forca.py
@@ -12,6 +12,10 @@ def pegando_palavras():
 
 
 def escolhendo_palavra_aleatoria():    
+    """
+    def escolhendo_palavra_aleatoria -> Escolhe uma palavra aleatória da base de palavras
+    return palavra escolhida randomicamente 
+    """
     from random import choice  
     palavras = pegando_palavras()
     palavra_sorteada = choice(palavras)
@@ -19,7 +23,24 @@ def escolhendo_palavra_aleatoria():
 
 
 def tratando_palavra_sorteda(): 
+    """
+    tratando_palavra_sorteda -> pega a palavra sorteada e a trata 
+    return Uma palavra 
+    """
+    palavra = escolhendo_palavra_aleatoria()
+    palavra_underline = ["_" for x in range(len(palavra))]
+    return palavra, palavra_underline
 
 
+def jogando(palavra, palavra_modicificada):
+    # chutando uma letra
+    print()
+    
 
 
+def apresentando_regra(): 
+    print("jogo da forca".upper())
+    print("""Regras: \n1 - Você tera 6 chances de acertar a palavra \n1.1 - Se acertar a palavra dentro das 6 chances você ganha o jogo \n1.2 - Se não você perde 
+    """)
+
+apresentando_regra()
